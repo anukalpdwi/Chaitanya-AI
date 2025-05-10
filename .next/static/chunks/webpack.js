6ab5d9ec93550ff66ac1b2a0eb54267d3c0a7b04
Anukalp Dwivedi <anukalpdwi1995@gmail.com>
2025-05-10 16:09:44 +0530

    vercel error fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "28746cbc437a431b"; }
+/******/ 		__webpack_require__.h = function() { return "0ba81b2632a780a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
